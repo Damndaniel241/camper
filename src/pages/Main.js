aaec87212270e3593e9c644d241837ca62ead1ec
Damndaniel241 <lombardia241@gmail.com>
2024-06-20 23:22:27 -0700

    changed requirements.txt to requirement.txt.bak

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -534,78 +534,6 @@ function Main() {
                   < RiLogoutCircleRLine/>
                   Logout
                 </div>
-
-              
-                
-               
-                {/* <div
-                  class="modal fade"
-                  id="modalId2"
-                  tabindex="-1"
-                  data-bs-backdrop="static"
-                  data-bs-keyboard="false"
-                  
-                  role="dialog"
-                  aria-labelledby="modalTitleId"
-                  aria-hidden="true"
-                >
-                  <div
-                    class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
-                    role="document"
-                  >
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="modalTitleId">
-                          Logout
-                        </h5>
-                        <button
-                          type="button"
-                          class="btn-close"
-                          data-bs-dismiss="modal"
-                          aria-label="Close"
-                        ></button>
-                      </div>
-                      <div class="modal-body">
-                  Are you sure you want to logout?
-fgbnrghgbfbdfbfbfg
-                      </div>
-                      <div class="modal-footer">
-                        <button
-                          type="button"
-                          class="btn btn-secondary"
-                          data-bs-dismiss="modal"
-                        >
-                          No
-                        </button>
-                        <button  onClick={logout} type="button" class="btn btn-primary">Yes</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                
-             
-                <script>
-                  const myModal = new bootstrap.Modal(
-                    document.getElementById("logout"),
-                    options,
-                  );
-                </script> */}
-                
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
               </li>
             </ul>
           </div>
@@ -635,10 +563,12 @@ fgbnrghgbfbdfbfbfg
                       />
                     </div>
 
-                    <div className="mb-3 d-flex flex-column gap-2">
-                      <label for="" className="form-label">
+                    <div className="mb-3 d-flex flex-column gap-2 ">
+                    <label for="" className="form-label">
                         Password
                       </label>
+                      <div className="d-flex ">
+                    
                       <input
                         type={isPasswordVisible ? "text" : "password"}
                         className="form-control form-control-sm"
@@ -649,6 +579,17 @@ fgbnrghgbfbdfbfbfg
                         onChange={(e) => setPassword(e.target.value)}
                         value={password}
                       />
+
+   
+<span className="password-toggle  align-self-center " onClick={togglePasswordVisibility}>
+                        {isPasswordVisible ? (
+                        <FaEye/>
+                        ) : (
+                        <FaEyeSlash/>
+                        )}
+                        </span>
+</div>
+</div>
 
                       <PasswordValidator
                         rules={[
@@ -672,7 +613,7 @@ fgbnrghgbfbdfbfbfg
                       />
 
                       <button
-                        className="btn btn-primary align-self-start"
+                        className="btn btn-primary align-self-start my-3"
                         onClick={(e) => {
                           e.preventDefault();
                           generatePassword();
@@ -681,7 +622,7 @@ fgbnrghgbfbdfbfbfg
                         Generate Password
                       </button>
                      
-                    </div>
+                    
 
                     <div class="mb-3">
                       <label for="" class="form-label">
