61c657e64e1dca96dde445bc9448e8d8daf5f310
Damndaniel241 <lombardia241@gmail.com>
2024-06-22 19:31:33 -0700

    changed settings.py

--- core/settings.py
+++ core/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,9 +146,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
-# MEDIA_ROOT  = BASE_DIR / 'media'
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
